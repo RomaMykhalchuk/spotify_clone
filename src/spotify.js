ca69be6e07e9fc6a9fedc9432561aa6dba8b5721
Roma <roman.mykhalchuk@yahoo.com>
2020-09-13 12:05:34 +0300

    working on auth

---
+++ src/spotify.js
@@ -0,0 +1,18 @@
+//e250c14aef2e40e5b027154f126f9920
+
+export const authEndpoint = "http://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "e250c14aef2e40e5b027154f126f9920";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
