a6319b03823203de54458336bf85c997ca855f2d
Roma <roman.mykhalchuk@yahoo.com>
2020-09-13 18:29:28 +0300

    footer part

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import PlayCircleFilledWhiteIcon from "@material-ui/icons/PlayCircleFilledWhite";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import Slider from '@material-ui/core/Slider';
+import VolumeDown from '@material-ui/icons/VolumeDown';
+import VolumeUp from '@material-ui/icons/VolumeUp';
+import Grid from '@material-ui/core/Grid';
 
 export const Footer = () => {
   return (
     <div className="footer">
-      footer
+      <div className="footer__left">
+        <h1>Album and song details</h1>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon"/>
+        <PlayCircleFilledWhiteIcon fontSize="large" className="footer__icon"  />
+        <SkipNextIcon className="footer__icon" />
+
+        <RepeatIcon className="footer__green"/>
+      </div>
+      <div className="footer__right">
+      <Grid container spacing={2}>
+        <Grid item>
+          <VolumeDown />
+        </Grid>
+        <Grid item xs>
+          <Slider />
+        </Grid>
+        <Grid item>
+          <VolumeUp />
+        </Grid>
+      </Grid>
+      </div>
     </div>
   );
 };
--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
   playing: false,
   item: null,
   //remove
-  // token: 'BQC8QW8vRO75QyENnd25bwmlxI0jYGjACNiYpo5M_wBYTOg-oRLmjAKHSsMIGjWiYLfqNugqt336lvSwn0SAP-uVa7XlZo--3tqdCf_mUho_f5RsHeZtmjQ3QZrGOHweQRb8EruTjjAidBkquG-ErQAGNvPb3U4M4Qkh91K5OlXHvFzL',
+  token: 'BQC8QW8vRO75QyENnd25bwmlxI0jYGjACNiYpo5M_wBYTOg-oRLmjAKHSsMIGjWiYLfqNugqt336lvSwn0SAP-uVa7XlZo--3tqdCf_mUho_f5RsHeZtmjQ3QZrGOHweQRb8EruTjjAidBkquG-ErQAGNvPb3U4M4Qkh91K5OlXHvFzL',
 };
 
 const reducer = (state, action) => {
