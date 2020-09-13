a1b150646061d99e28b9a214abfba583c779214a
Roma <roman.mykhalchuk@yahoo.com>
2020-09-13 12:33:05 +0300

    got token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-import {Login} from  './Login';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Login } from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log(token);
+  }, []);
+
   return (
     //BEM
     <div className="app">
-       <Login />
+      <Login />
     </div>
   );
 }
