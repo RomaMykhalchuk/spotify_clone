2dac83ac936010aa15a16b96d790b8ba32219eef
Roma <roman.mykhalchuk@yahoo.com>
2020-09-23 16:02:26 +0300

    Updates

---
+++ precache-manifest.0b8441dab5cf4681c1e7d72e50dac9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68e6ec5f23036e5c0e0d1800df18a3c",
+    "url": "/RomaMykhalchuk/spotify_clone/index.html"
+  },
+  {
+    "revision": "2e1217d2d73605db276b",
+    "url": "/RomaMykhalchuk/spotify_clone/static/css/main.abac47fb.chunk.css"
+  },
+  {
+    "revision": "f4a5a67a520c92ed8688",
+    "url": "/RomaMykhalchuk/spotify_clone/static/js/2.00ea6584.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RomaMykhalchuk/spotify_clone/static/js/2.00ea6584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1217d2d73605db276b",
+    "url": "/RomaMykhalchuk/spotify_clone/static/js/main.fe65130a.chunk.js"
+  },
+  {
+    "revision": "ccfc11841b2f2897e34e",
+    "url": "/RomaMykhalchuk/spotify_clone/static/js/runtime-main.d235aabe.js"
+  }
+]);
